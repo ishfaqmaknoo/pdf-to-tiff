7b13af58c096d3fa63f23cc295cf1a90d86b6834
ishfaqmaknoo <ishfaqmaknoo12@gmail.com>
2026-01-04 15:35:25 +0530

    feat: improvise code

--- convert.sh
+++ convert.sh
@@ -3,7 +3,6 @@
 
 INPUT_FILE=$1
 OUTPUT_FILE=$2
-DENSITY=${3:-300} # Default 300 DPI
+DENSITY=${3:-300}  # Default 300 DPI
 
-
-convert "$INPUT_FILE" -density "$DENSITY" "$OUTPUT_FILE"
+magick "$INPUT_FILE" -density "$DENSITY" "$OUTPUT_FILE"
